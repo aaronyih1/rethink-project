e3334ede2ad2d284690c7bddebba94db37027147
Aaron Yih <aaronyih@Aarons-MacBook-Pro-2.local>
2020-01-08 08:55:54 -0800

    finsihed letter, fixed undefined word error

--- pages/index/PlaintextEditor/index.js
+++ pages/index/PlaintextEditor/index.js
@@ -1,13 +1,40 @@
 /* 
 Hi Will, 
 
-This was the simplist way to implement a plain text editor 
-with the structure of the project as is. My solution
-could be improved in a number of ways if I had more time:
+This was the simplist way to implement a plain text editor. 
+My solution could be improved in a number of ways if I had
+more time:
 
   1.) Modify the existing file instead of creating a new one every time
-  2.) Save on a specific time interval (say every 10 seconds) and give the user a button to save manually
+  2.) Save on a specific time interval (say every 10 seconds) and give the user a button to save manually to avoid saving after every change
+
+
+An alternative solution would have been to avoid text area
+completely and build my own text editor interface from scratch.
+It would have taken longer, but it would have also given us
+more control around the rendering of the text (the textArea 
+value attr does not accept html). Since the challenge was about 
+rendering "plain text," I decided to focus on building functionality
+around plain text instead of converting the doc to rich text or
+displaying text in funky ways, though that would probably be fun too.
+
+There are a few libraries like Draft.js that can make that approach
+really straightforward.
+
+Aside from making the plaintext files editable, I also:
+
+  1.) sorted files by thir last edit date
+  2.) added a pig latin text translator
+  3.) added a dictionary lookup feature
+
+I felt that these ideas demonstrated some interesting ways to expand
+on viewing and editing plaintext docs in rethink. This is especially
+true if you consider the possibility of using a real language 
+translator (like french or mandarin) instead of pig latin and
+pulling in interesting data from all kinds of other data, like a 
+Wikipedia entries.
 */
+
 import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 var piglatin = require('piglatin');
@@ -115,11 +142,17 @@ function PlaintextEditor({ file, write }) {
             //     console.log(result.status, result.headers, result.body);
             //   });
             //window.getSelection().focusNode.setAttribute('data-tip', "Hello this is a datatip");
-            if(wordData.success==false){
-              setDefinition("error: "+wordData.message)
+            // if(wordData.success==false || typeof wordData.results[0] != "undefined"){
+            //   setDefinition("error: "+wordData.message)
+            // }
+            // else{
+            //   setDefinition(selectedWord+" : "+wordData.results[0].definition)
+            // }
+            if(typeof wordData.results != "undefined"){
+              setDefinition(selectedWord+" : "+wordData.results[0].definition)
             }
             else{
-              setDefinition(wordData.results[0].definition)
+              setDefinition("error: "+wordData.message)
             }
             // console.log(window.getSelection())
           }
